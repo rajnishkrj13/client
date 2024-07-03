818dd1e6070a267308f75fd798c1dcd995627ad2
Rajnish <rajnishkrj13@gmail.com>
2024-07-03 17:57:08 +0530

    Added New Printing and user ID

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-
-
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,51 @@
+// import React, { useEffect, useState } from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
+// import './Navbar.css';
+// import { useUser } from './UserContext';
+
+// const Navbar = ({ isAuthenticated, handleLogout }) => {
+//   const [isAdmin, setIsAdmin] = useState(false);
+//   const navigate = useNavigate();
+//   const { user } = useUser();
+
+//   useEffect(() => {
+//     const isAdminUser = localStorage.getItem('isAdmin');
+//     setIsAdmin(isAdminUser === 'true');
+//   }, [user]);
+  
+
+//   const onLogoutClick = () => {
+//     localStorage.clear();
+//     handleLogout();
+//     navigate('/login');
+//   };
+
+//   return (
+//     <nav className="navbar">
+//       <Link to="/" className="navbar-brand">JO DMS</Link>
+//       <ul className="navbar-links">
+//         {!isAuthenticated ? (
+//           <>
+//             <li><Link to="/login">Login</Link></li>
+//             <li><Link to="/register">Register</Link></li>
+//           </>
+//         ) : (
+//           <>
+            // <li><Link to="/">Dashboard</Link></li>
+            // <li><Link to="/stage1">Stage1</Link></li>
+            // <li><Link to="/stage2">Stage2</Link></li>
+            // <li><Link to="/stage3">Stage3</Link></li>
+            // <li><Link to="/stage4">Stage4</Link></li>
+//             {isAdmin  && <li><Link to="/printing">Printing</Link></li>}
+//             <li><button onClick={onLogoutClick}>Logout</button></li>
+//           </>
+//         )}
+//       </ul>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Navbar.css';
@@ -31,12 +79,23 @@ const Navbar = ({ isAuthenticated, handleLogout }) => {
           </>
         ) : (
           <>
-            <li><Link to="/">Dashboard</Link></li>
-            <li><Link to="/stage1">Stage1</Link></li>
-            <li><Link to="/stage2">Stage2</Link></li>
-            <li><Link to="/stage3">Stage3</Link></li>
-            <li><Link to="/stage4">Stage4</Link></li>
-            {isAdmin  && <li><Link to="/printing">Printing</Link></li>}
+            {
+            
+            user && user.email === 'treta@justorganik.com' ? (
+              <>
+                <li><Link to="/register">Register</Link></li>
+                <li><Link to="/printing">Printing</Link></li>
+              </>
+            ) : (
+              <>
+                <li><Link to="/">Dashboard</Link></li>
+                <li><Link to="/stage1">Stage1</Link></li>
+                <li><Link to="/stage2">Stage2</Link></li>
+                <li><Link to="/stage3">Stage3</Link></li>
+                <li><Link to="/stage4">Stage4</Link></li>
+                {isAdmin && <li><Link to="/printing">Printing</Link></li>}
+              </>
+            )}
             <li><button onClick={onLogoutClick}>Logout</button></li>
           </>
         )}
